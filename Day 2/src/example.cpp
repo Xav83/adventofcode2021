3897909980b1c49d85c53cc86617579940ade3c1
Xav83 <x.jouvenot@gmail.com>
2021-12-07 22:25:46 +0100

    Adds a simple program which solves the first part of the day 2 example.

---
+++ Day 2/src/example.cpp
@@ -0,0 +1,28 @@
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+constexpr std::array<std::string_view, 6> input = { "forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2" };
+
+constexpr bool isForwardInstruction (const std::string_view instruction) { return instruction.substr(0, 7) == "forward" ;}
+constexpr bool isDownInstruction (const std::string_view instruction) { return instruction.substr(0, 4) == "down" ;}
+constexpr bool isUpInstruction (const std::string_view instruction) { return instruction.substr(0, 2) == "up" ;}
+
+int main()
+{
+    size_t horizontalPosition{0}, depth{0};
+
+    for(const auto& instruction : input)
+    {
+        if(isForwardInstruction(instruction)) { horizontalPosition += std::stoi(instruction.substr(8).data()); continue; }
+        if(isDownInstruction(instruction)) { depth += std::stoi(instruction.substr(5).data()); continue; }
+        if(isUpInstruction(instruction)) { depth -= std::stoi(instruction.substr(3).data()); continue; }
+        assert(false); // Unkwnow input
+    }
+
+    std::cout << "The solution is:" << (horizontalPosition * depth) << std::endl;
+
+    return 0;
+}
