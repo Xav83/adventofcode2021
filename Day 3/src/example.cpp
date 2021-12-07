bc271833c9834ffa5a4c4141d766899d077a6acc
Xav83 <x.jouvenot@gmail.com>
2021-12-08 00:17:31 +0100

    Adds a simple program which solve the example of the second part of the day 3 of Advent of Code

--- Day 3/src/example.cpp
+++ Day 3/src/example.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <array>
 #include <cassert>
 #include <cmath>
@@ -13,7 +14,7 @@ struct Counter
     size_t numberOfOne{0};
 };
 
-int main()
+void firstPart()
 {
     auto gammaRate{0}, epsilonRate{0};
     std::array<Counter, input.front().size()> counters;
@@ -39,5 +40,97 @@ int main()
     }
 
     std::cout << "The solution is: " << (gammaRate * epsilonRate) << std::endl;
+}
+
+size_t binaryToDecimal(const std::string_view binary)
+{
+    auto decimalNumber{0};
+    for(auto index = 0; index < binary.size(); ++index)
+    {
+        const auto& figure = binary[index];
+        const auto powerOfTwo = binary.size() - 1 - index;
+        if(figure == '1') { decimalNumber += std::pow(2, powerOfTwo); continue; }
+    }
+    return decimalNumber;
+}
+
+void secondPart()
+{
+    size_t oxygenGeneratorRating{0}, co2ScrubberRating{0};
+
+    std::array<Counter, input.front().size()> counters;
+
+    std::vector<std::string_view> potentialsOxygenGeneratorRating(input.begin(), input.end());
+    std::vector<std::string_view> potentialsCo2ScrubberRating(input.begin(), input.end());
+
+    for(auto index = 0; index < input.front().size(); ++index)
+    {
+        Counter co2Counter, o2Counter;
+        for(const auto& binary: potentialsCo2ScrubberRating)
+        {
+            const auto figure = binary[index];
+            if(figure == '0') { ++(co2Counter.numberOfZero) ; continue; }
+            if(figure == '1') { ++(co2Counter.numberOfOne) ; continue; }
+            assert(false); // Wrong input
+        }
+
+        for(const auto& binary: potentialsOxygenGeneratorRating)
+        {
+            const auto figure = binary[index];
+            if(figure == '0') { ++(o2Counter.numberOfZero) ; continue; }
+            if(figure == '1') { ++(o2Counter.numberOfOne) ; continue; }
+            assert(false); // Wrong input
+        }
+
+        if(potentialsCo2ScrubberRating.size() != 1)
+        {
+            if(co2Counter.numberOfZero > co2Counter.numberOfOne)
+            {
+                /* removes the binaries with a 0 in this position for CO2 */;
+                potentialsCo2ScrubberRating.erase(std::remove_if(potentialsCo2ScrubberRating.begin(), potentialsCo2ScrubberRating.end(),
+                                                                    [index](const auto& binary){ return binary[index] == '0'; }),
+                                                    potentialsCo2ScrubberRating.end());
+            }
+            else
+            {
+                /* removes the binaries with a 1 in this position for CO2 */;
+                potentialsCo2ScrubberRating.erase(std::remove_if(potentialsCo2ScrubberRating.begin(), potentialsCo2ScrubberRating.end(),
+                                                                    [index](const auto& binary){ return binary[index] == '1'; }),
+                                                    potentialsCo2ScrubberRating.end());
+            }
+        }
+
+        if(potentialsOxygenGeneratorRating.size() != 1)
+        {
+            if(o2Counter.numberOfZero > o2Counter.numberOfOne)
+            {
+                /* removes the binaries with a 1 in this position for O2 */;
+                potentialsOxygenGeneratorRating.erase(std::remove_if(potentialsOxygenGeneratorRating.begin(), potentialsOxygenGeneratorRating.end(),
+                                                                    [index](const auto& binary){ return binary[index] == '1'; }),
+                                                    potentialsOxygenGeneratorRating.end());
+            }
+            else
+            {
+                /* removes the binaries with a 0 in this position for O2 */;
+                potentialsOxygenGeneratorRating.erase(std::remove_if(potentialsOxygenGeneratorRating.begin(), potentialsOxygenGeneratorRating.end(),
+                                                                    [index](const auto& binary){ return binary[index] == '0'; }),
+                                                    potentialsOxygenGeneratorRating.end());
+            }
+        }
+    }
+
+    assert(potentialsCo2ScrubberRating.size() == 1);
+    assert(potentialsOxygenGeneratorRating.size() == 1);
+
+    oxygenGeneratorRating = binaryToDecimal(potentialsOxygenGeneratorRating.front());
+    co2ScrubberRating = binaryToDecimal(potentialsCo2ScrubberRating.front());
+
+    std::cout << "The solution is: " << (oxygenGeneratorRating * co2ScrubberRating) << std::endl;
+}
+
+int main()
+{
+    firstPart();
+    secondPart();
     return 0;
 }
