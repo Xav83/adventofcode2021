879c4a230982ac3c8524521ac661b8de782df528
Xav83 <x.jouvenot@gmail.com>
2021-12-07 22:39:17 +0100

    Adds a simple program which solves the example of the second part of the day 2.

--- Day 2/src/example.cpp
+++ Day 2/src/example.cpp
@@ -10,7 +10,7 @@ constexpr bool isForwardInstruction (const std::string_view instruction) { retur
 constexpr bool isDownInstruction (const std::string_view instruction) { return instruction.substr(0, 4) == "down" ;}
 constexpr bool isUpInstruction (const std::string_view instruction) { return instruction.substr(0, 2) == "up" ;}
 
-int main()
+void firstPart()
 {
     size_t horizontalPosition{0}, depth{0};
 
@@ -23,6 +23,33 @@ int main()
     }
 
     std::cout << "The solution is: " << (horizontalPosition * depth) << std::endl;
+}
 
+void secondPart()
+{
+    size_t horizontalPosition{0}, depth{0};
+    long aim{0};
+
+    for(const auto& instruction : input)
+    {
+        if(isDownInstruction(instruction)) { aim += std::stoi(instruction.substr(5).data()); continue; }
+        if(isUpInstruction(instruction)) { aim -= std::stoi(instruction.substr(3).data()); continue; }
+        if(isForwardInstruction(instruction))
+        {
+            const auto units = std::stoi(instruction.substr(8).data());
+            horizontalPosition += units;
+            depth += units * aim;
+            continue;
+        }
+        assert(false); // Unkwnow input
+    }
+
+    std::cout << "The solution is: " << (horizontalPosition * depth) << std::endl;
+}
+
+int main()
+{
+    firstPart();
+    secondPart();
     return 0;
 }
