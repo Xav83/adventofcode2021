f95736f3013f39974051fc48070e626e9386a311
Xav83 <x.jouvenot@gmail.com>
2022-01-27 23:06:34 +0100

    Adds a simple program which solve the example the second part of the day 5 of Advent of Code

--- Day 5/src/example.cpp
+++ Day 5/src/example.cpp
@@ -32,6 +32,7 @@ Line {{5,5}, {8,2}},
 
 constexpr bool isVertical(const Line& line) { return line.start.y == line.end.y; }
 constexpr bool isHorizontal(const Line& line) { return line.start.x == line.end.x; }
+constexpr bool isDiagonal(const Line& line) { return std::abs(line.start.x - line.end.x) == std::abs(line.start.y - line.end.y); }
 
 constexpr int getGridSize() {
     int max{0};
@@ -71,8 +72,48 @@ void firstPart()
     std::cout << "The solution is: " << numberOfDangerousAreas << std::endl;
 }
 
+void secondPart()
+{
+    std::array<std::array<int, getGridSize()>, getGridSize()> grid{0};
+    for(const auto& line : input_lines)
+    {
+        if(isVertical(line))
+        {
+            auto[min, max] = std::minmax(line.start.x, line.end.x);
+            for(int i = min; i<= max; ++i)
+            {
+                ++grid[i][line.start.y];
+            }
+        }
+        if(isHorizontal(line))
+        {
+            auto[min, max] = std::minmax(line.start.y, line.end.y);
+            for(int i = min; i<= max; ++i)
+            {
+                ++grid[line.start.x][i];
+            }
+        }
+        if(isDiagonal(line))
+        {
+            auto lineSize = std::abs(line.start.x - line.end.x);
+            auto xdirection = (line.start.x - line.end.x) < 0 ? 1 : -1 ;
+            auto ydirection = (line.start.y - line.end.y) < 0 ? 1 : -1 ;
+            for(int i = 0; i<= lineSize; ++i)
+            {
+                ++grid[line.start.x + (i * xdirection)][line.start.y + (i * ydirection)];
+            }
+        }
+    }
+
+    const auto numberOfDangerousAreas = std::accumulate(std::begin(grid), std::end(grid), 0, [](const auto& sum, const auto& line){
+       return sum + std::count_if(std::begin(line), std::end(line), [](const auto& point){ return point >= 2; }); 
+    });
+    std::cout << "The solution is: " << numberOfDangerousAreas << std::endl;
+}
+
 int main()
 {
     firstPart();
+    secondPart();
     return 0;
 }
