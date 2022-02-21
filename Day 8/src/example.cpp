d1386f762b3dbc6547ae9f8f73b886e750663d31
Xav83 <x.jouvenot@gmail.com>
2022-02-21 19:21:38 +0100

    Adds a simple program which solve the example of the second part of the day 8 of Advent of Code

--- Day 8/src/example.cpp
+++ Day 8/src/example.cpp
@@ -13,25 +13,88 @@ class SignalPattern
 public:
     constexpr SignalPattern(std::string_view message_) : message(message_) {}
 
+    bool operator==(const SignalPattern& other) const {
+        auto messageCopy = std::string(message);
+        auto otherMessageCopy = std::string(other.message);
+
+        std::sort(std::begin(messageCopy), std::end(messageCopy));
+        std::sort(std::begin(otherMessageCopy), std::end(otherMessageCopy));
+
+        return messageCopy == otherMessageCopy;
+    }
+
+    int getNumberOfCommonSegment(const SignalPattern& other) const {
+        return std::accumulate(std::begin(message), std::end(message), 0, [&other](const auto sum, const auto& c){ return sum + (other.message.find(c) != std::string_view::npos ? 1 : 0); });
+    }
+
     constexpr bool isNumber1() const { return message.size() == 2; }
     constexpr bool isNumber4() const { return message.size() == 4; }
     constexpr bool isNumber7() const { return message.size() == 3; }
     constexpr bool isNumber8() const { return message.size() == 7; }
 
-private:
+    constexpr bool isNumber0_6_or_9() const { return message.size() == 6; }
+    constexpr bool isNumber2_3_or_5() const { return message.size() == 5; }
+
+//private:
     std::string_view message;
 };
 
 struct Message
 {
     int getNumberOf1_4_7_8_inOuputs() const {
-        return std::accumulate(std::begin(ouputValues), std::end(ouputValues), 0, [](const auto sum, const auto& signalPattern){
+        return std::accumulate(std::begin(outputValues), std::end(outputValues), 0, [](const auto sum, const auto& signalPattern){
             return sum + ((signalPattern.isNumber1() or signalPattern.isNumber4() or signalPattern.isNumber7() or signalPattern.isNumber8()) ? 1 : 0);
         });
     }
 
+    void orderTestValues() {
+        for(auto i=0; i<testValues.size(); ++i) {
+            if(testValues[i].isNumber1()) { std::swap(testValues[i], testValues[1]); if(i < 1) { --i; } }
+            else if(testValues[i].isNumber4()) { std::swap(testValues[i], testValues[4]); if(i < 4) { --i; } }
+            else if(testValues[i].isNumber7()) { std::swap(testValues[i], testValues[7]); if(i < 7) { --i; } }
+            else if(testValues[i].isNumber8()) { std::swap(testValues[i], testValues[8]); if(i < 8) { --i; } }
+        }
+        for(auto i=0; i<testValues.size(); ++i) {
+            if(testValues[i].isNumber2_3_or_5()) { 
+                if(testValues[i].getNumberOfCommonSegment(testValues[1]) == 2) {
+                    std::swap(testValues[i], testValues[3]);
+                    if(i < 3) { --i; }
+                    continue;
+                }
+                else if(testValues[i].getNumberOfCommonSegment(testValues[4]) == 2) {
+                    std::swap(testValues[i], testValues[2]);
+                    if(i < 2) { --i; }
+                    continue;
+                }
+                else {
+                    std::swap(testValues[i], testValues[5]);
+                    if(i < 5) { --i; }
+                }
+            }
+        }
+        for(auto i=0; i<testValues.size(); ++i) {
+            if(testValues[i].isNumber0_6_or_9()) { 
+                if(testValues[i].getNumberOfCommonSegment(testValues[3]) == 5) {
+                    std::swap(testValues[i], testValues[9]);
+                    if(i < 9) { --i; }
+                    continue;
+                }
+                if(testValues[i].getNumberOfCommonSegment(testValues[5]) == 4) { 
+                    std::swap(testValues[i], testValues[0]);
+                    continue;
+                }
+                if(testValues[i].getNumberOfCommonSegment(testValues[1]) == 1) {
+                    std::swap(testValues[i], testValues[6]);
+                    if(i < 6) { --i; }
+                    continue;
+                }
+                assert(false);
+            }
+        }
+    }
+
     std::array<SignalPattern, 10> testValues;
-    std::array<SignalPattern, 4> ouputValues;
+    std::array<SignalPattern, 4> outputValues;
 };
 
 constexpr std::array<Message, 10> input {
@@ -54,8 +117,29 @@ void firstPart()
     std::cout << "The solution is: " << result << std::endl;
 }
 
+void secondPart()
+{
+    auto messages = input;
+    for(auto& message : messages) { message.orderTestValues(); }
+
+    auto result{0};
+    for(const auto& message: messages) {
+        std::string value;
+        for(const auto& outputValue : message.outputValues) {
+            auto it = std::find(std::begin(message.testValues), std::end(message.testValues), outputValue);
+            assert(it != std::end(message.testValues));
+            auto number = std::distance(std::begin(message.testValues), it);
+            value += std::to_string(number);
+        }
+        result += std::stoi(value);
+    }
+
+    std::cout << "The solution is: " << result << std::endl;
+}
+
 int main()
 {
     firstPart();
+    secondPart();
     return 0;
 }
